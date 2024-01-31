4dc173f0149d48b1448aab5715f72cc4822f2e52
Ava Dominie <avadominie@gmail.com>
2024-01-31 11:48:45 -0600

    UserProfile Adopted Animals

--- client/src/components/userProfile/UserProfileDetails.js
+++ client/src/components/userProfile/UserProfileDetails.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { getUserProfileById } from "../../managers/UserProfileManager";
+import { Link } from "react-router-dom";
 
 
 
@@ -16,16 +17,33 @@ export default function UserProfileDetails({ loggedInUser }) {
     }, [getUserId]);
 
 
-    
+
     console.log("user name", user)
+
     return (
         <div>
-            <h2>Profile Details</h2>
+            <h2>User Profile Details</h2>
             {user ? (
                 <>
                     <p>Full Name: {user.fullName}</p>
                     <p>Email: {user.email}</p>
                     <p>Address: {user.address}</p>
+
+                    <h3>Adopted Animals:</h3>
+                    {user.adoptions && user.adoptions.length > 0 ? (
+                        <ul>
+                            {user.adoptions.map((adoption) => (
+                                <li key={adoption.id}>
+                                    <p>Name: {adoption.animal.name}</p>
+                                    <Link to={`/animalDetails/${adoption.animal.id}`} >
+                                    <p><img src={adoption.animal.urlPic} alt={adoption.animal.name} /></p>
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    ) : (
+                        <p>No adopted animals.</p>
+                    )}
                 </>
             ) : (
                 <p>Loading...</p>
